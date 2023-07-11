cbbc62922dff0f44a9f183036e9c8ed7ea01cc44
Thamimtommy <116333932+Thamimtommy@users.noreply.github.com>
2023-07-11 18:12:41 +0530

    update on day02
    Some minute mistake I fixed it.

--- src/main/java/day02/Solved/Main.java
+++ src/main/java/day02/Solved/Main.java
@@ -1,8 +1,12 @@
 package day02.Solved;
+
 public class Main {
     public static void main(String[] args) {
         ShapeCalculator calculator = new ShapeCalculator();
-        double circleArea = calculator.calculateArea(3.5);                     // Output: 38.48451000647496
-        double rectangleArea = calculator.calculateArea(4.2, 6.8);            // Output: 28.559999999999995
+        double circleArea = calculator.calculateArea(3.5);  
+        System.out.println(circleArea);// Output: 38.48451000647496
+        double rectangleArea = calculator.calculateArea(4.2, 6.8); 
+        System.out.println(rectangleArea);
+        // Output: 28.559999999999995
     }
 }
